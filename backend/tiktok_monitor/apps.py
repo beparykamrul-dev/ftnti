b8193c121a50bbf1a91ae48ad26339d3b9ea7e1f
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 23:35:42 -0600

    Add TikTok monitor app configuration

---
+++ backend/tiktok_monitor/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TikTokMonitorConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tiktok_monitor"
+    verbose_name = "FTN TikTok Integration"
